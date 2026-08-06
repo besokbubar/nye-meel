ac5cc9d218be0d65b92ea9be7b3a11b94024a6f3
besokbubar <besokbubar100x@gmail.com>
2026-08-06 07:41:13 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 "use client";
 
 import React, { useState } from "react";
-import Layout from "../layout";
-import KasirTab from "../components/KasirTab";
-import BarangTab from "../components/BarangTab";
-import HutangTab from "../components/HutangTab";
-import LaporanTab from "../components/LaporanTab";
+// MENGGUNAKAN ALIAS COMPONENT RESMI KITA (@/components/Layout)
+import Layout from "@/components/Layout";
+import KasirTab from "@/components/KasirTab";
+import BarangTab from "@/components/BarangTab";
+import HutangTab from "@/components/HutangTab";
+import LaporanTab from "@/components/LaporanTab";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState("kasir");
